2779b323d56b6f93598bb8d92035ef1b9404de8d
ammeum <98023937+ammeum@users.noreply.github.com>
2022-11-10 11:45:05 +0100

    Add files via upload

---
+++ samleside/myscript.js
@@ -0,0 +1,11 @@
+function mobilMenu(){
+
+  var x = document.getElementById("ham");
+
+  if(x.style.display === "block") {
+      x.style.display = "none";
+  }
+   else {
+      x.style.display = "block";
+  }
+}
